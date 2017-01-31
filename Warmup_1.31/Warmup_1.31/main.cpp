78e9a6eb9b7198280ba18c9ff4e2d28d64f3bde8
game110pm <game110pm@605-110-STU19>
2017-01-31 12:39:08 -0700

    Warmup for Jan 31

---
+++ Warmup_1.31/Warmup_1.31/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int subtract;
+	cout << "how much money do you want to take out?" << endl;
+	cin >> subtract;
+	cout << "you know have" << 1000 - subtract;
---
+++ warmupmon/warmupmon/Source.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int subtract;
+	cout << "how much money do you want to take out?" << endl;
+	cin >> subtract;
+	cout << "you know have" << 1000 - subtract;
+}
